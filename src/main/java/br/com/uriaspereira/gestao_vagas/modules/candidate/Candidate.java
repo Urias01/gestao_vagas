621a436e84e7999240aa1c8d44afd4b75f0a0056
Urias Pereira <uriasluis64@gmail.com>
2024-07-01 08:54:58 -0300

    validate fields

--- src/main/java/br/com/uriaspereira/gestao_vagas/modules/candidate/Candidate.java
+++ src/main/java/br/com/uriaspereira/gestao_vagas/modules/candidate/Candidate.java
@@ -1,6 +1,9 @@
 package br.com.uriaspereira.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,8 +12,14 @@ public class Candidate {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaços")
     private String username;
+
+    @Email(message = "O campo [email] deve ter um e-mail válido")
     private String email;
+
+    @Length(min = 10, max = 100)
     private String password;
     private String description;
     private String curriculum;
--- src/main/java/br/com/uriaspereira/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/uriaspereira/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,7 @@
 package br.com.uriaspereira.gestao_vagas.modules.candidate.controllers;
 
 import br.com.uriaspereira.gestao_vagas.modules.candidate.Candidate;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody Candidate candidate) {
+    public void create(@Valid @RequestBody Candidate candidate) {
         System.out.println("Candidato");
         System.out.println(candidate.getEmail());
     }
