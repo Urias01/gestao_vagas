dd1ba320672f03ab060b8e16c5681e54976f0d83
Urias Pereira <uriasluis64@gmail.com>
2024-07-01 09:53:45 -0300

    criando tabela candidates

--- src/main/java/br/com/uriaspereira/gestao_vagas/modules/candidate/Candidate.java
+++ src/main/java/br/com/uriaspereira/gestao_vagas/modules/candidate/Candidate.java
@@ -1,18 +1,30 @@
 package br.com.uriaspereira.gestao_vagas.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidates")
 public class Candidate {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
+    @NotBlank
     @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaços")
     private String username;
 
@@ -23,6 +35,9 @@ public class Candidate {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
     public Candidate(
             String name,
